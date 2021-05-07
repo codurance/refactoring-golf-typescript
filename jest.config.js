1a5c79e47f0a2c8e17d6ba27309d9519119d5ab2
Javier Chacana <javier.chacana@codurance.com>
2021-05-07 09:22:35 +0200

    Updated configs

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    preset: 'ts-jest',
+    // collectCoverage: true,
+    // collectCoverageFrom: ['./src/**/*.{js,jsx,ts}', '!**/node_modules/**', '!**/vendor/**'],
+  }
