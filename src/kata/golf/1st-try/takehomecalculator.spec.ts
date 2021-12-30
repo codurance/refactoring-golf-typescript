ffdcf1571ca05f754cdc0b7daac1ffbbe679c957
gnunez <gnunez@aidacanarias.com>
2021-12-30 08:29:36 +0000

    refactor

--- src/kata/golf/1st-try/takehomecalculator.spec.ts
+++ src/kata/golf/1st-try/takehomecalculator.spec.ts
@@ -1,14 +1,14 @@
 import {Takehomecalculator} from "./takehomecalculator";
-import { Money } from "./money";
+import { money } from "./money";
 import { taxRate } from "./TaxRate";
 
 describe('TakeHomeCalculator', () => {
     it("can calculate tax", () => {
-        const first: number = new Takehomecalculator(taxRate(10)).netAmount(new Money(40, "GBP"), new Money(50, "GBP"), new Money(60, "GBP")).value;
+        const first: number = new Takehomecalculator(taxRate(10)).netAmount(money(40, "GBP"), money(50, "GBP"), money(60, "GBP")).value;
         expect(Math.trunc(first)).toBe(135)
     })
 
     it("cannot sum different currencies", () => {
-        expect(() => new Takehomecalculator(taxRate(10)).netAmount(new Money(4, "GBP"), new Money(5, "USD"))).toThrow()
+        expect(() => new Takehomecalculator(taxRate(10)).netAmount(money(4, "GBP"), money(5, "USD"))).toThrow()
     })
 })
